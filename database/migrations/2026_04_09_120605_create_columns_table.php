6c4cbab2b3e56c9efe86b113b171b5b436f8b09e
andrei-zglavoc <andrei.zglavoc@gmail.com>
2026-04-09 15:38:35 +0300

    added migrations

---
+++ database/migrations/2026_04_09_120605_create_columns_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('columns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('board_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('name');
+            $table->foreignId('after_column_id')
+                ->nullable()
+                ->constrained('columns')
+                ->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('columns');
+    }
+};
---
+++ database/migrations/2026_04_09_120621_create_cards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('column_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('after_card_id')
+                ->nullable()
+                ->constrained('cards')
+                ->nullOnDelete();
+            $table->string('name');
+            $table->text('text')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
