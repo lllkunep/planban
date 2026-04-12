8f6c01a80cdfc18814b96d0370f03c7fcf7dea2f
andrei zglavoc <andrei.zglavoc@gmail.com>
2026-04-12 17:46:21 +0300

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Board;
+use App\Models\Card;
+use App\Models\Column;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +20,108 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name'     => 'Test User',
+            'email'    => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $board1 = Board::create([
+            'name'          => 'Разработка проекта'
+        ]);
+
+        $board1->members()->attach($user->id, ['role' => 'owner']);
+
+        $col1 = Column::create([
+            'board_id'        => $board1->id,
+            'name'            => 'To do',
+            'after_column_id' => null,
+        ]);
+
+        $col2 = Column::create([
+            'board_id'        => $board1->id,
+            'name'            => 'In process',
+            'after_column_id' => $col1->id,
+        ]);
+
+        $col3 = Column::create([
+            'board_id'        => $board1->id,
+            'name'            => 'Done',
+            'after_column_id' => $col2->id,
+        ]);
+
+        $card1 = Card::create([
+            'column_id'     => $col1->id,
+            'name'          => 'Настроить окружение',
+            'text'          => 'Установить Docker, настроить Sail, создать базу данных.',
+            'after_card_id' => null,
+        ]);
+
+        $card2 = Card::create([
+            'column_id'     => $col1->id,
+            'name'          => 'Написать миграции',
+            'text'          => 'Создать таблицы boards, columns, cards и остальные.',
+            'after_card_id' => $card1->id,
+        ]);
+
+        $card3 = Card::create([
+            'column_id'     => $col2->id,
+            'name'          => 'Реализовать политики доступа',
+            'text'          => 'BoardPolicy, ColumnPolicy, CardPolicy, CommentPolicy.',
+            'after_card_id' => null,
+        ]);
+
+        $card4 = Card::create([
+            'column_id'     => $col2->id,
+            'name'          => 'Написать контроллеры',
+            'text'          => 'BoardController, ColumnController, CardController.',
+            'after_card_id' => $card3->id,
+        ]);
+
+        $card5 = Card::create([
+            'column_id'     => $col3->id,
+            'name'          => 'Настроить авторизацию',
+            'text'          => 'Laravel Breeze с Inertia + Vue.',
+            'after_card_id' => null,
+        ]);
+
+        $board2 = Board::create([
+            'name'          => 'Личные задачи'
+        ]);
+
+        $board2->members()->attach($user->id, ['role' => 'owner']);
+
+        $col4 = Column::create([
+            'board_id'        => $board2->id,
+            'name'            => 'Сделать',
+            'after_column_id' => null,
+        ]);
+
+        $col5 = Column::create([
+            'board_id'        => $board2->id,
+            'name'            => 'Сделано',
+            'after_column_id' => $col4->id,
+        ]);
+
+        $card6 = Card::create([
+            'column_id'     => $col4->id,
+            'name'          => 'Купить продукты',
+            'text'          => null,
+            'after_card_id' => null,
+        ]);
+
+        $card7 = Card::create([
+            'column_id'     => $col4->id,
+            'name'          => 'Записаться к врачу',
+            'text'          => null,
+            'after_card_id' => $card6->id,
+        ]);
+
+        $card8 = Card::create([
+            'column_id'     => $col5->id,
+            'name'          => 'Оплатить счета',
+            'text'          => null,
+            'after_card_id' => null,
         ]);
     }
 }
