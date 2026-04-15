a9254357afee314875c73614d540eadfdc394eb0
andrei zglavoc <andrei.zglavoc@gmail.com>
2026-04-15 19:44:10 +0300

    little thinks

--- resources/js/Components/Board/BoardListItem.vue
+++ resources/js/Components/Board/BoardListItem.vue
@@ -1,7 +1,6 @@
 <script setup>
-
 import {Link} from "@inertiajs/vue3";
-defineProps({
+const props = defineProps({
     board: {
         type: Object,
         required: true,
--- resources/js/Components/Board/CardItem.vue
+++ resources/js/Components/Board/CardItem.vue
@@ -1,5 +1,5 @@
 <script setup>
-defineProps({
+const props = defineProps({
     card: {
         type: Object,
         required: true,
