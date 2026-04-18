7786c8e19ac9a5d67bf336a2d1f59f55305cceab
andrei zglavoc <andrei.zglavoc@gmail.com>
2026-04-18 16:19:06 +0300

    edited board sidebar

--- resources/js/Components/Board/Sidebar.vue
+++ resources/js/Components/Board/Sidebar.vue
@@ -3,6 +3,7 @@
 import BoardListItem from "@/Components/Board/BoardListItem.vue";
 import { computed } from 'vue'
 import { usePage } from '@inertiajs/vue3'
+import Button from "@/Components/Common/Button.vue";
 
 const page = usePage()
 
@@ -15,13 +16,22 @@ const currentBoard = defineProps({
 <template>
     <div class="collapse collapse-horizontal show vh-100 bg-light py-4 px-3" id="collapseWidthExample">
         <div class="list-group" style="width: 300px;">
-            <a href="#" class="list-group-item list-group-item-action border-0">
-                Notifications
-            </a>
-            <hr>
-            <a href="#" class="list-group-item list-group-item-action border-0">Add user to board</a>
-            <hr>
-            <h5 class="ms-3">Board List:</h5>
+            <Button variant="light" class="w-100 list-group-item list-group-item-action border-0">
+                <i class="bi bi-bell"></i> Notifications
+            </Button>
+            <Button variant="light" class="w-100 list-group-item list-group-item-action border-0">
+                <i class="bi bi-person-add"></i> Add user to board
+            </Button>
+            <Button variant="light" class="w-100 list-group-item list-group-item-action border-0" :href="route('boards.edit', currentBoard.boardId)">
+                <i class="bi bi-gear"></i> Board settings
+            </Button>
+            <Button variant="light" class="w-100 list-group-item list-group-item-action border-0" :href="route('boards.edit', currentBoard.boardId)">
+                <i class="bi bi-clipboard-plus"></i> Create new board
+            </Button>
+        </div>
+        <hr>
+        <h5 class="ms-3">Board List:</h5>
+        <div class="list-group" style="width: 300px;">
             <BoardListItem v-for="board in boards" :key="board.id" :board="board" class="border-0" :active="board.id === currentBoard.boardId" />
         </div>
     </div>
