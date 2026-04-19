a7699912036985e99b4f4a1fce9a6be8de41a013
andrei zglavoc <andrei.zglavoc@gmail.com>
2026-04-19 16:26:52 +0300

    added methods in Board

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Board extends Model
 {
@@ -29,5 +30,24 @@ class Board extends Model
     public function invitations(): HasMany
     {
         return $this->hasMany(Invitation::class);
+    }
+
+    public function hasUser(User $user): bool
+    {
+        return $this->members()->where('user_id', $user->id)->exists();
+    }
+
+    public function setNewOwner(User $user)
+    {
+        DB::transaction(function () use ($user) {
+            DB::table('board_user')
+                ->where('board_id', $this->id)
+                ->where('role', 'owner')
+                ->update(['role' => 'admin']);
+
+            $this->members()->updateExistingPivot($user->id, [
+                'role' => 'owner',
+            ]);
+        });
     }
 }
